c4afe624009c0a0f12f6a08666cb2de957757156
ndakalajulius <julius.ndakala@student.moringaschool>
2022-08-17 06:08:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+multiply = num1 * num2;
+let random = Math.floor(Math.random()+1);
+ let num3 = 24;
+ let num4= 5;
+ let mod = num3 % num4;
+ let max = Math.max(15,16,16,17,18,19,20)
